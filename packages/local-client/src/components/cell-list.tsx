0647d1ac76c06c8cf747a963ea9d2375bbbb005e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2021-08-14 10:47:33 +0200

    Implement saving a list of cells

--- packages/local-client/src/components/cell-list.tsx
+++ packages/local-client/src/components/cell-list.tsx
@@ -1,15 +1,21 @@
 import './cell-list.css';
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selector';
 import CellListItem from './cell-list-item';
 import AddCell from './add-cell';
+import { useActions } from '../hooks/use-actions';
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) =>
-    order.map((id) => data[id])
+    order.map(id => data[id])
   );
+  const { fetchCells } = useActions();
 
-  const renderedCells = cells.map((cell) => (
+  useEffect(() => {
+    fetchCells();
+  }, []);
+
+  const renderedCells = cells.map(cell => (
     <Fragment key={cell.id}>
       <CellListItem cell={cell} />
       <AddCell previousCellId={cell.id} />
